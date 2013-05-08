c6c5ebba23c26ff493744009b751965444c7121f
Alexander Surma <surma@surmair.de>
2013-05-08 20:40:35 +0200

    Make s3 listing work

--- app/scripts/s3.js
+++ app/scripts/s3.js
@@ -7,21 +7,20 @@ define(['jquery', 'crypto'], function($, Crypto) {
 		this.headers = {
 			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
 		};
-		this.date = new Date().toString();
+		this.date = new Date().toGMTString();
 		this.content = '';
 		this.request_parameters = {};
 
 		this.sign = function(key, secret) {
 			this.headers['Authorization'] = 'AWS '+key+':'+this.signature(secret);
+			console.log(this.headers['Authorization']);
 		};
 		this.signature = function(secret) {
-			btoa(Crypto.HMAC(Crypto.SHA1, this._toSign(), secret, {asString: true}));
-		};
-		this.canonicalResource = function() {
-			return '/'+this.bucketname+'/'+this.path;
+			return btoa(Crypto.HMAC(Crypto.SHA1, this._toSign(), secret, {asString: true}));
 		};
 		this.make_request = function() {
 			this.headers["Host"] = this.hostname;
+			this.headers["x-amz-date"] = this.date;
 			return $.ajax(this._url(), {
 				contentType: this.headers["Content-Type"],
 				crossDomain: true,
@@ -31,12 +30,26 @@ define(['jquery', 'crypto'], function($, Crypto) {
 		};
 
 		this._toSign = function() {
-			return this.verb.toUpperCase()+'\n'+
-			Crypto.MD5(this.content)+'\n'+
-			this.headers['Content-Type']+'\n'+
-			this.date+'\n'+
-			this._url(true)+'\n'+
-			this.canonicalResource();
+			return [
+				this.verb.toUpperCase(),
+				this._content_hash(),
+				this.headers['Content-Type'],
+				"",
+				this._canonicalized_amz_headers(),
+				this._canonicalized_resource()
+			].join('\n');
+		};
+		this._content_hash= function() {
+			if(this.verb != 'POST' && this.verb != 'PUT') {
+				return ""
+			}
+			return Crypto.MD5(this.content);
+		}
+		this._canonicalized_resource = function() {
+			return '/'+this.bucketname+'/'+this.path;
+		};
+		this._canonicalized_amz_headers = function() {
+			return 'x-amz-date:'+this.date;
 		};
 		this._url = function(strip_query) {
 			var url = 'https://'+this.host+'/'+this.bucketname+'/'+this.path;
