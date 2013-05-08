87d802e357ccb85a79301c7b8f24865c331b8e9c
Alexander Surma <surma@surmair.de>
2013-05-08 20:57:41 +0200

    Implemented put

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,4 @@ define(['s3'], function (S3) {
     'use strict';
 
     var bucket = new S3('s3-eu-west-1.amazonaws.com', 'elasticpw.surmair.de', 'AKIAI63P7EOLGUSUJM5A', '54emaFuRkPnJFWtZ+r3qB0R7ihTgZYjHDWCPJb29');
-    bucket.list('/', function(data) {
-        console.log(data);
-    })
 });
--- app/scripts/s3.js
+++ app/scripts/s3.js
@@ -5,7 +5,7 @@ define(['jquery', 'crypto'], function($, Crypto) {
 		this.bucketname = '';
 		this.path = '';
 		this.headers = {
-			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
 		};
 		this.date = new Date().toGMTString();
 		this.content = '';
@@ -13,16 +13,16 @@ define(['jquery', 'crypto'], function($, Crypto) {
 
 		this.sign = function(key, secret) {
 			this.headers['Authorization'] = 'AWS '+key+':'+this.signature(secret);
-			console.log(this.headers['Authorization']);
 		};
 		this.signature = function(secret) {
 			return btoa(Crypto.HMAC(Crypto.SHA1, this._toSign(), secret, {asString: true}));
 		};
 		this.make_request = function() {
-			this.headers["Host"] = this.hostname;
-			this.headers["x-amz-date"] = this.date;
+			this.headers['Host'] = this.hostname;
+			this.headers['x-amz-date'] = this.date;
 			return $.ajax(this._url(), {
-				contentType: this.headers["Content-Type"],
+				type: this.verb,
+				contentType: this.headers['Content-Type'],
 				crossDomain: true,
 				data: this.content,
 				headers: this.headers,
@@ -32,27 +32,21 @@ define(['jquery', 'crypto'], function($, Crypto) {
 		this._toSign = function() {
 			return [
 				this.verb.toUpperCase(),
-				this._content_hash(),
-				this.headers['Content-Type'],
 				"",
+				this.headers['Content-Type'],
+				'',
 				this._canonicalized_amz_headers(),
 				this._canonicalized_resource()
 			].join('\n');
 		};
-		this._content_hash= function() {
-			if(this.verb != 'POST' && this.verb != 'PUT') {
-				return ""
-			}
-			return Crypto.MD5(this.content);
-		}
 		this._canonicalized_resource = function() {
-			return '/'+this.bucketname+'/'+this.path;
+			return '/'+this.bucketname+this.path;
 		};
 		this._canonicalized_amz_headers = function() {
 			return 'x-amz-date:'+this.date;
 		};
 		this._url = function(strip_query) {
-			var url = 'https://'+this.host+'/'+this.bucketname+'/'+this.path;
+			var url = 'https://'+this.host+'/'+this.bucketname+this.path;
 			if(strip_query) {
 				return url;
 			}
@@ -77,13 +71,28 @@ define(['jquery', 'crypto'], function($, Crypto) {
 				req.host = endpoint;
 				req.bucketname = this.bucketname;
 				if(prefix) {
-					req.request_parameters["prefix"] = prefix;
+					req.request_parameters['prefix'] = prefix;
 				}
-				req.sign(key, secret);
+				req.sign(this.key, secret);
 				req.make_request().success(function(data) {
 					cb(data);
 				}).error(function(data) {
-					cb({error: data});
+					cb(null);
+				});
+			},
+
+			put: function(key, content, cb) {
+				var req = new Request();
+				req.verb = 'PUT';
+				req.host = endpoint;
+				req.path = key;
+				req.bucketname = this.bucketname;
+				req.content = content;
+				req.sign(this.key, secret);
+				req.make_request().success(function(data) {
+					cb(data);
+				}).error(function(data) {
+					cb(null);
 				});
 			}
 		}
